57708584f04bcc7bacc72902f9572fc567fedb46
MrSpigot <220609166+Sebastian-Vetter@users.noreply.github.com>
2025-09-06 21:15:27 +0200

    added api response, updated postcontroller, postservice and post repository

---
+++ src/response/api.response.ts
@@ -0,0 +1,10 @@
+export class ApiResponse<T> {
+    message!: string;
+    data!: T;
+
+    constructor(message: string, data: T) {
+        this.message = message;
+        this.data = data;
+    }
+
+}
