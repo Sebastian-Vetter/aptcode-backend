526504fcb242785251f77b7a09399ad3662ccc2e
MrSpigot <220609166+Sebastian-Vetter@users.noreply.github.com>
2025-09-06 18:31:19 +0200

    added next methode for error handling

--- src/routes/post.route.ts
+++ src/routes/post.route.ts
@@ -1,22 +1,22 @@
 //postsRoute to manage requests and response
-import express from 'express';
+import express, {NextFunction} from 'express';
 import {createPost, readPost, deletePost, updatePost} from "../controllers/post.controller";
 import {Request, Response} from "express";
 
 const router = express.Router();
 //get post route
-router.get('/:id', async (req: Request, res: Response) => {
-    return res.json(await readPost(req, res));
+router.get('/:id', async (req: Request, res: Response, next: NextFunction) => {
+    return res.json(await readPost(req, res, next));
 })
 //delete post route
-router.delete('/:id', async (req: Request, res: Response) => {
-    return res.json(await deletePost(req, res));
+router.delete('/:id', async (req: Request, res: Response, next: NextFunction) => {
+    return res.json(await deletePost(req, res, next));
 })
 //create post route
-router.post('/:id', async (req: Request, res: Response) => {
-    return res.json(await createPost(req, res));
+router.post('/:id', async (req: Request, res: Response, next: NextFunction) => {
+    return res.json(await createPost(req, res, next));
 })
 //update post route
-router.patch('/:id', async (req: Request, res: Response) => {
-    return res.json(await updatePost(req, res));
+router.patch('/:id', async (req: Request, res: Response, next: NextFunction) => {
+    return res.json(await updatePost(req, res, next));
 })
