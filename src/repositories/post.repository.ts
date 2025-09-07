70801531225de4f55f34b56155bc7cb86199cd82
MrSpigot <220609166+Sebastian-Vetter@users.noreply.github.com>
2025-09-07 10:03:25 +0200

    added error.middleware.ts

--- src/repositories/post.repository.ts
+++ src/repositories/post.repository.ts
@@ -1,23 +1,22 @@
 import {PostRepositoryInterface} from "../interfaces/post.repository.interface";
 import {PostType} from "../types/post.type";
 import {postModel} from "../models/post.model";
-import e from "express";
 import {PostAlreadyExistsError} from "../error/post.already.exists.error";
 import {PostNotUpdatedError} from "../error/post.not.updated.error";
 
 export class PostRepository implements PostRepositoryInterface {
 
-    // Alle Posts abrufen
+    // get all posts
     public async findAll(): Promise<PostType[]> {
         return postModel.find({});
     }
 
-    // Einen Post nach ID abrufen
+    // find post by id
     public async findById(id: string): Promise<PostType | null> {
         return postModel.findOne({id: id});
     }
 
-    // Post erstellen
+    // create post
     public async createPost(post: PostType): Promise<PostType> {
         const createdPost = new postModel(post);
         if (await createdPost.save()) {
@@ -27,7 +26,7 @@ export class PostRepository implements PostRepositoryInterface {
         }
     }
 
-    // Post updaten
+    // update post
     public async update(id: string, update: PostType): Promise<PostType> {
         const updatedPost = await postModel.findOneAndUpdate(
             {id},
@@ -41,13 +40,13 @@ export class PostRepository implements PostRepositoryInterface {
         return update;
     }
 
-    // Post löschen
+    // delete post
     public async delete(id: string): Promise<boolean> {
         const result = await postModel.deleteOne({id});
         return result.deletedCount > 0;
     }
 
-    // Prüfen, ob Post existiert
+    // check if post exists with id
     public async exists(id: string): Promise<boolean> {
         const post = await postModel.exists({id});
         return post !== null;
